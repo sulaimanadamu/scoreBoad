36b5d18cf4b69468fc315d54b6c88db4c2d3f89f
sulaiman247 <pablourban247@gmail.com>
2023-03-31 16:39:24 +0100

    scoreBoard app created

---
+++ index.js
@@ -0,0 +1,29 @@
+let homeScore = 0;
+let guestScore = 0;
+
+function hadd1(){
+    homeScore += 1
+   document.getElementById("home").textContent = homeScore
+}
+function hadd2(){
+     homeScore += 2
+   document.getElementById("home").textContent = homeScore
+}
+function hadd3(){
+     homeScore += 3
+   document.getElementById("home").textContent = homeScore
+}
+
+function gadd1(){
+    guestScore += 1
+   document.getElementById("guest").textContent = guestScore
+}
+function gadd2(){
+     guestScore += 2
+   document.getElementById("guest").textContent = guestScore
+}
+function gadd3(){
+     guestScore += 3
+   document.getElementById("guest").textContent = guestScore
+}
+
